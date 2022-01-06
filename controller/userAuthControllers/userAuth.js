32643d7efe17b6c2c1b7dfdfab6b0d223f5d737d
Manoj1234321 <manojdreams2017@gmail.com>
2022-01-06 15:49:21 +0530

    changed file structure and modified task id

---
+++ controller/userAuthControllers/userAuth.js
@@ -0,0 +1,29 @@
+const {
+    default: axios
+  } = require("axios");
+  
+  let userAuth = async (req, res) => {
+    try {
+      const token = req.headers.authorization.split(" ")[1];
+      var image = await axios.get(
+        "https://graph.microsoft.com/v1.0/me/photo/$value", {
+          responseType: "arraybuffer",
+          headers: {
+            Authorization: "Bearer " + token,
+          },
+        }
+      );
+      const buffer = Buffer.from(image.data, "binary").toString("base64");
+      req.dataFromMiddleware1["image"] = buffer;
+      res.send(req.dataFromMiddleware1);
+    } catch (err) {
+      if (err.response.status == 404) {
+        req.dataFromMiddleware1["image"] = " ";
+        res.status(404).send(req.dataFromMiddleware1);
+      }
+    }
+  };
+  
+  module.exports = {
+    userAuth,
+  };
---
+++ model/calenderModels/calender.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const calenderSchema = new mongoose.Schema({
+
+    userid: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: String,
+        required: true,
+    },
+    data: [{
+        taskid: Number,
+        startTime: String,
+        endTime: String,
+        summary: String,
+    }, ],
+    created_at: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+//  Creating the model
+
+let Calender = new mongoose.model("Calender", calenderSchema);
+
+module.exports = Calender;
