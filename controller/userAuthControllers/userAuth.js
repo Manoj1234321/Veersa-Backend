f28c7e9b314ce20bac8f13c19a73e71a72f66a17
Manoj1234321 <manojdreams2017@gmail.com>
2022-01-12 15:31:00 +0530

    isAdmin and isViewer functionality done

--- controller/userAuthControllers/userAuth.js
+++ controller/userAuthControllers/userAuth.js
@@ -1,29 +1,45 @@
-const {
-    default: axios
-  } = require("axios");
-  
-  let userAuth = async (req, res) => {
-    try {
-      const token = req.headers.authorization.split(" ")[1];
-      var image = await axios.get(
-        "https://graph.microsoft.com/v1.0/me/photo/$value", {
-          responseType: "arraybuffer",
-          headers: {
-            Authorization: "Bearer " + token,
-          },
-        }
-      );
-      const buffer = Buffer.from(image.data, "binary").toString("base64");
-      req.dataFromMiddleware1["image"] = buffer;
-      res.send(req.dataFromMiddleware1);
-    } catch (err) {
-      if (err.response.status == 404) {
-        req.dataFromMiddleware1["image"] = " ";
-        res.send(req.dataFromMiddleware1);
+const { default: axios } = require("axios");
+
+let userAuth = async (req, res) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    var image = await axios.get(
+      "https://graph.microsoft.com/v1.0/me/photo/$value",
+      {
+        responseType: "arraybuffer",
+        headers: {
+          Authorization: "Bearer " + token,
+        },
       }
+    );
+    const buffer = Buffer.from(image.data, "binary").toString("base64");
+    req.dataFromMiddleware1["image"] = buffer;
+    if (req.dataFromMiddleware1.jobTitle == "Software Engineer") {
+      req.dataFromMiddleware1["isAdmin"] = false;
+      req.dataFromMiddleware1["isviewer"] = true;
     }
-  };
-  
-  module.exports = {
-    userAuth,
-  };
+    if (req.dataFromMiddleware1.jobTitle == "isAdmin") {
+      req.dataFromMiddleware1["isAdmin"] = true;
+      req.dataFromMiddleware1["isviewer"] = true;
+    }
+
+    res.send(req.dataFromMiddleware1);
+  } catch (err) {
+    if (err.response.status == 404) {
+      req.dataFromMiddleware1["image"] = " ";
+      if (req.dataFromMiddleware1.jobTitle == "Software Engineer") {
+        req.dataFromMiddleware1["isAdmin"] = false;
+        req.dataFromMiddleware1["isviewer"] = true;
+      }
+      if (req.dataFromMiddleware1.jobTitle == "isAdmin") {
+        req.dataFromMiddleware1["isAdmin"] = true;
+        req.dataFromMiddleware1["isviewer"] = true;
+      }
+      res.send(req.dataFromMiddleware1);
+    }
+  }
+};
+
+module.exports = {
+  userAuth,
+};
--- routes/calenderRoutes/calender.js
+++ routes/calenderRoutes/calender.js
@@ -3,11 +3,10 @@ const app = express();
 const middlewares = require("../../middleware/userAuthmiddleware");
 const calender = require("../../controller/calenderControllers/calender");
 
-
 /**
  * @swagger
  *  components:
- *      schemas: 
+ *      schemas:
  *          Pet:
  *              type: object
  *              required:
@@ -16,30 +15,33 @@ const calender = require("../../controller/calenderControllers/calender");
  *                pet_type:
  *                  type: string
  *              discriminator:
- *                propertyName: pet_type  
+ *                propertyName: pet_type
  *          Dog:
  *            allOf:
  *              - $ref: '#/components/schemas/Pet'
  *              - type: object
  *                properties:
  *                 date:
- *                   type: string 
+ *                   type: string
+ *                   example: 12-05-2021
  *          Cat:
  *            allOf:
  *              - $ref: '#components/schemas/Pet'
  *              - type: array
  *                properties:
- *                    taskid: 
+ *                    taskid:
  *                      type: string
+ *                    example: 4
  *                    startTime:
  *                      type: string
+ *                      example: 10:10
  *                    endTime:
  *                      type: string
+ *                      example: 11:00
  *                    summary:
- *                      type: string      
+ *                      type: string
+ *                      example: created api for get route
  */
-
-
 
 /**
  * @swagger
@@ -58,7 +60,7 @@ const calender = require("../../controller/calenderControllers/calender");
  *          200:
  *              description: success
  */
-app.post('/calender', middlewares.userAuthenticationMiddleware, calender.store);
+app.post("/calender", middlewares.userAuthenticationMiddleware, calender.store);
 
 /**
  * @swagger
@@ -66,7 +68,7 @@ app.post('/calender', middlewares.userAuthenticationMiddleware, calender.store);
  *  get:
  *   parameters:
  *     - in: query
- *       name: date 
+ *       name: date
  *   summary: get Data of the particular user by date
  *   description: get Data of the user's by date
  *   responses:
@@ -75,6 +77,6 @@ app.post('/calender', middlewares.userAuthenticationMiddleware, calender.store);
  *    500:
  *     description: error
  */
-app.get('/calender', middlewares.userAuthenticationMiddleware, calender.index);
+app.get("/calender", middlewares.userAuthenticationMiddleware, calender.index);
 
 module.exports = app;
